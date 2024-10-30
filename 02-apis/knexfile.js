feb28f4301a4552b8878af2ce6c0f2b23104cc16
lucasribeiiroo <lucasribeiiroo@hotmail.com>
2024-10-30 08:57:08 -0300

    Remoção de arquivos js

--- 02-apis/knexfile.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./src/database");
-const config = database_1.config;
-exports.default = config;
