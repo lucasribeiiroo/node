d780018585d7561d4102629b9306322aac59942b
lucasribeiiroo <lucasribeiiroo@hotmail.com>
2024-10-30 08:44:19 -0300

    Feat: deploy configs

---
+++ 02-apis/knexfile.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./src/database");
+const config = database_1.config;
+exports.default = config;
