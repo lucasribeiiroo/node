27a34cfd520704965bee068af98ea7d9bf6f4154
lucasribeiiroo <lucasribeiiroo@hotmail.com>
2024-11-06 10:44:18 -0300

    Feat: remoção de console

--- 03-solid/src/http/controllers/middlewares/verify-user.role.ts
+++ 03-solid/src/http/controllers/middlewares/verify-user.role.ts
@@ -4,8 +4,6 @@ export function verifyUserRole(roleToVerify: "ADMIN" | "MEMBER") {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user;
 
-    console.log("ROLE------", role);
-
     if (role !== roleToVerify) {
       return reply.status(401).send({ message: "Unauthorized." });
     }
